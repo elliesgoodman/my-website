303d3603310f3477d20b29cd0c69b8b099ab6f9e
Ellie <Eleanor@students-air-2.home>
2019-04-02 15:42:13 +0100

    Added Nav Wrapper
    Added a Nav wrapper to try and get JS going...

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").height = "30px";
+  } else {
+    document.getElementById("logo").height = "50px";
+  }
+}
